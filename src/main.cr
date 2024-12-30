9a371b2d981867a4d4c2540ac65e7f72b799b9cc
Andrea Mancuso <and.mancuso@googlemail.com>
2024-12-30 12:39:23 +0100

    Update main.cr

--- src/main.cr
+++ src/main.cr
@@ -1,16 +1,27 @@
 require "json"
 
-font_defs = {
-  "defs" => [
-    {"name" => "roboto-regular", "sizes" => [16, 18, 20, 24, 28, 32, 36, 48]}
-  ].map do |entry|
-    name = entry["name"].as(String)
-    sizes = entry["sizes"].as(Array(Int32))
-    sizes.map do |size|
-      {"name" => name, "size" => size}
+# Define a struct for the font definition
+struct FontDef
+    getter name : String
+    getter sizes : Array(Int32)
+
+    def initialize(name : String, sizes : Array(Int32))
+        @name = name
+        @sizes = sizes
     end
-  end.flatten
+end
+
+# Create the font_defs array with FontDef instances
+font_defs = {
+    "defs" => [
+        FontDef.new("roboto-regular", [16, 18, 20, 24, 28, 32, 36, 48])
+    ].map do |font_def|
+        font_def.sizes.map do |size|
+        {"name" => font_def.name, "size" => size}
+        end
+    end.flatten
 }
+  
 
 # Convert to JSON
 json_string = font_defs.to_json
